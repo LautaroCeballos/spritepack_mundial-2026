c831a8caa9115fe3ec449976d8cd34c6d9d21a1c
Code_and_beers <lautaroceballos2013@gmail.com>
2026-06-24 14:42:22 -0300

    Nuevos diseños

--- images.g.ts
+++ images.g.ts
@@ -3,14 +3,109 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "image10":
+            case "miImagen":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+. . . . 1 1 2 1 1 2 1 1 . . . . 
+. . . 1 1 2 1 1 1 1 2 1 1 . . . 
+. . . 1 2 1 1 2 2 1 1 2 1 . . . 
+. . 1 1 1 1 2 1 1 2 1 1 1 1 . . 
+. . 1 2 1 2 1 1 1 1 2 1 2 1 . . 
+. . 1 1 2 1 1 2 2 1 1 2 1 1 . . 
+. . 1 1 2 1 1 2 2 1 1 2 1 1 . . 
+. . 1 2 1 2 1 1 1 1 2 1 2 1 . . 
+. . 1 1 1 1 2 1 1 2 1 1 1 1 . . 
+. . . 1 2 1 1 2 2 1 1 2 1 . . . 
+. . . 1 1 2 1 1 1 1 2 1 1 . . . 
+. . . . 1 1 2 1 1 2 1 1 . . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image11":
+            case "miImagen0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 6 6 6 6 6 6 . . . . . 
+. . . . 6 6 5 6 6 5 6 6 . . . . 
+. . . 6 6 5 6 6 6 6 5 6 6 . . . 
+. . . 6 5 6 6 5 5 6 6 5 6 . . . 
+. . 6 6 6 6 5 6 6 5 6 6 6 6 . . 
+. . 6 5 6 5 6 6 6 6 5 6 5 6 . . 
+. . 6 6 5 6 6 5 5 6 6 5 6 6 . . 
+. . 6 6 5 6 6 5 5 6 6 5 6 6 . . 
+. . 6 5 6 5 6 6 6 6 5 6 5 6 . . 
+. . 6 6 6 6 5 6 6 5 6 6 6 6 . . 
+. . . 6 5 6 6 5 5 6 6 5 6 . . . 
+. . . 6 6 5 6 6 6 6 5 6 6 . . . 
+. . . . 6 6 5 6 6 5 6 6 . . . . 
+. . . . . 6 6 6 6 6 6 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image12":
+            case "miImagen1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 9 9 9 9 9 9 . . . . . 
+. . . . 9 9 1 9 9 1 9 9 . . . . 
+. . . 9 9 1 9 9 9 9 1 9 9 . . . 
+. . . 9 1 9 9 a a 9 9 1 9 . . . 
+. . 9 9 9 9 1 9 9 1 9 9 9 9 . . 
+. . 9 1 9 1 9 9 9 9 1 9 1 9 . . 
+. . 9 9 1 9 9 1 1 9 9 1 9 9 . . 
+. . 9 9 1 9 9 1 1 9 9 1 9 9 . . 
+. . 9 1 9 1 9 9 9 9 1 9 1 9 . . 
+. . 9 9 9 9 1 9 9 1 9 9 9 9 . . 
+. . . 9 1 9 9 a a 9 9 1 9 . . . 
+. . . 9 9 1 9 9 9 9 1 9 9 . . . 
+. . . . 9 9 1 9 9 1 9 9 . . . . 
+. . . . . 9 9 9 9 9 9 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image13":
+            case "miImagen2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . 7 7 8 7 7 8 7 7 . . . . 
+. . . 7 7 8 7 7 7 7 8 7 7 . . . 
+. . . 7 8 7 7 1 1 7 7 8 7 . . . 
+. . 7 7 7 7 8 7 7 8 7 7 7 7 . . 
+. . 7 8 7 8 7 7 7 7 8 7 8 7 . . 
+. . 7 7 8 7 7 8 8 7 7 8 7 7 . . 
+. . 7 7 8 7 7 8 8 7 7 8 7 7 . . 
+. . 7 8 7 8 7 7 7 7 8 7 8 7 . . 
+. . 7 7 7 7 8 7 7 8 7 7 7 7 . . 
+. . . 7 8 7 7 1 1 7 7 8 7 . . . 
+. . . 7 7 8 7 7 7 7 8 7 7 . . . 
+. . . . 7 7 8 7 7 8 7 7 . . . . 
+. . . . . 7 7 7 7 7 7 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image14":
+            case "miImagen3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+. . . . 1 1 6 1 1 6 1 1 . . . . 
+. . . 1 1 6 1 1 1 1 6 1 1 . . . 
+. . . 1 6 1 9 6 6 9 1 6 1 . . . 
+. . 1 1 1 9 6 1 1 6 9 1 1 1 . . 
+. . 1 6 9 6 1 1 1 1 6 9 6 1 . . 
+. . 1 1 6 1 1 8 8 1 1 6 1 1 . . 
+. . 1 1 6 1 1 8 8 1 1 6 1 1 . . 
+. . 1 6 9 6 1 1 1 1 6 9 6 1 . . 
+. . 1 1 1 9 6 1 1 6 9 1 1 1 . . 
+. . . 1 6 1 9 6 6 9 1 6 1 . . . 
+. . . 1 1 6 1 1 1 1 6 1 1 . . . 
+. . . . 1 1 6 1 1 6 1 1 . . . . 
+. . . . . 1 1 1 1 1 1 . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
             case "image1":
             case "player_argentina":return img`
 . . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
+. . . . f f f 9 9 f f f . . . . 
+. . . f f f 9 9 9 9 f f f . . . 
+. . f f f 1 1 1 1 1 1 f f f . . 
+. . f f 1 9 9 9 9 9 9 1 e f . . 
+. . f 1 9 9 f f f f 9 9 1 f . . 
 . . f f f f e e e e f f f f . . 
 . f f e f b f 4 4 f b f e f f . 
 . f e e 4 1 f d d f 1 4 e e f . 
@@ -25,11 +120,11 @@ namespace myImages {
             case "image2":
             case "player_brasil":return img`
 . . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
+. . . . f f f 5 5 f f f . . . . 
+. . . f f f 5 5 5 5 f f f . . . 
+. . f f f 7 7 7 7 7 7 f f f . . 
+. . f f 7 5 5 5 5 5 5 7 e f . . 
+. . f 7 5 5 f f f f 5 5 7 f . . 
 . . f f f f e e e e f f f f . . 
 . f f e f b f 4 4 f b f e f f . 
 . f e e 4 1 f d d f 1 4 e e f . 
@@ -46,9 +141,9 @@ namespace myImages {
 . . . . . . f f f f . . . . . . 
 . . . . f f f 2 2 f f f . . . . 
 . . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
+. . f f f 8 8 8 8 8 8 f f f . . 
+. . f f 8 1 1 1 1 1 1 8 e f . . 
+. . f 8 2 2 f f f f 2 2 8 f . . 
 . . f f f f e e e e f f f f . . 
 . f f e f b f 4 4 f b f e f f . 
 . f e e 4 1 f d d f 1 4 e e f . 
@@ -65,9 +160,9 @@ namespace myImages {
 . . . . . . f f f f . . . . . . 
 . . . . f f f 2 2 f f f . . . . 
 . . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
+. . f f f c c c c c c f f f . . 
+. . f f c 5 5 5 5 5 5 c e f . . 
+. . f c 2 2 f f f f 2 2 c f . . 
 . . f f f f e e e e f f f f . . 
 . f f e f b f 4 4 f b f e f f . 
 . f e e 4 1 f d d f 1 4 e e f . 
@@ -82,11 +177,11 @@ namespace myImages {
             case "image5":
             case "player_españa":return img`
 . . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
+. . . . f f f 5 5 f f f . . . . 
+. . . f f f 5 5 5 5 f f f . . . 
+. . f f f 2 2 2 2 2 2 f f f . . 
+. . f f 2 5 5 5 5 5 5 2 e f . . 
+. . f 2 2 2 f f f f 2 2 2 f . . 
 . . f f f f e e e e f f f f . . 
 . f f e f b f 4 4 f b f e f f . 
 . f e e 4 1 f d d f 1 4 e e f . 
@@ -98,33 +193,14 @@ namespace myImages {
 . . . . . f f f f f f . . . . . 
 . . . . . f f . . f f . . . . . 
 `;
-            case "image6":
-            case "player_italia":return img`
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
-. . f f f f e e e e f f f f . . 
-. f f e f b f 4 4 f b f e f f . 
-. f e e 4 1 f d d f 1 4 e e f . 
-. . f e e d d d d d d e e f . . 
-. . . f e e 4 4 4 4 e e f . . . 
-. . e 4 f 8 8 8 8 8 8 f 4 e . . 
-. . 4 d f 8 8 8 8 8 8 f d 4 . . 
-. . 4 4 f 1 1 1 1 1 1 f 4 4 . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`;
             case "image7":
             case "player_japon":return img`
 . . . . . . f f f f . . . . . . 
 . . . . f f f 2 2 f f f . . . . 
 . . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
+. . f f f 1 1 1 1 1 1 f f f . . 
+. . f f 1 2 2 2 2 2 2 1 e f . . 
+. . f 1 1 1 f f f f 1 1 1 f . . 
 . . f f f f e e e e f f f f . . 
 . f f e f b f 4 4 f b f e f f . 
 . f e e 4 1 f d d f 1 4 e e f . 
@@ -139,11 +215,11 @@ namespace myImages {
             case "image8":
             case "player_mexico":return img`
 . . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
+. . . . f f f 1 1 f f f . . . . 
+. . . f f f 1 1 1 1 f f f . . . 
+. . f f f 7 7 7 7 7 7 f f f . . 
+. . f f 7 1 1 1 1 1 1 7 e f . . 
+. . f 7 2 2 f f f f 2 2 7 f . . 
 . . f f f f e e e e f f f f . . 
 . f f e f b f 4 4 f b f e f f . 
 . f e e 4 1 f d d f 1 4 e e f . 
@@ -155,22 +231,212 @@ namespace myImages {
 . . . . . f f f f f f . . . . . 
 . . . . . f f . . f f . . . . . 
 `;
-            case "image9":
-            case "player_paisesbajos":return img`
+            case "image15":
+            case "player_argentina_espalda":return img`
 . . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 1 1 f f f f f . . 
+. . f f e 9 e 1 1 e 9 e f f . . 
+. . f e 9 f 1 f f 1 f 9 e f . . 
+. . f f f 9 1 e e 1 9 f f f . . 
+. f f e f 9 f e e f 9 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 9 9 1 1 1 1 9 9 4 e . . 
+. . 4 d f 9 1 1 1 1 9 f d 4 . . 
+. . 4 4 f 9 1 1 1 1 9 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image16":
+            case "player_brasil_espalda":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 5 5 f f f f f . . 
+. . f f e 7 e 5 5 e 7 e f f . . 
+. . f e 7 f 5 f f 5 f 7 e f . . 
+. . f f f 7 5 e e 5 7 f f f . . 
+. f f e f 7 f e e f 7 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 7 7 7 7 5 7 7 7 4 e . . 
+. . 4 d f 7 7 5 8 5 7 f d 4 . . 
+. . 4 4 f 7 7 7 5 7 7 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image17":
+            case "player_francia_espalda":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 8 8 f f f f f . . 
+. . f f e 8 e 8 8 e 8 e f f . . 
+. . f e 8 f 8 f f 8 f 8 e f . . 
+. . f f f 8 8 e e 8 8 f f f . . 
+. f f e f 8 f e e f 8 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 8 8 1 1 2 2 2 2 4 e . . 
+. . 4 d f 8 1 1 2 2 2 f d 4 . . 
+. . 4 4 f 8 1 1 2 2 2 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image18":
+            case "player_alemania_espalda":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 5 5 f f f f f . . 
+. . f f e 2 e 5 5 e 2 e f f . . 
+. . f e 2 f 5 f f 5 f 2 e f . . 
+. . f f f 2 5 e e 5 2 f f f . . 
+. f f e f 2 f e e f 2 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 c c c c 2 2 2 2 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 5 5 5 5 5 5 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image19":
+            case "player_españa_espalda":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 5 5 f f f f f . . 
+. . f f e 2 e 5 5 e 2 e f f . . 
+. . f e 2 f 5 f f 5 f 2 e f . . 
+. . f f f 2 5 e e 5 2 f f f . . 
+. f f e f 2 f e e f 2 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 2 2 5 5 5 5 2 2 4 e . . 
+. . 4 d f 2 5 5 5 5 2 f d 4 . . 
+. . 4 4 f 2 2 5 5 2 2 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image21":
+            case "player_japon_espalda":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 2 2 f f f f f . . 
+. . f f e 2 e 2 2 e 2 e f f . . 
+. . f e 2 f 2 f f 2 f 2 e f . . 
+. . f f f 2 2 e e 2 2 f f f . . 
+. f f e f 2 f e e f 2 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 1 1 1 1 1 1 1 1 4 e . . 
+. . 4 d f 1 1 2 2 1 1 f d 4 . . 
+. . 4 4 f 1 1 1 1 1 1 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image22":
+            case "player_mexico_espalda":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 7 7 f f f f f . . 
+. . f f e 7 e 7 7 e 7 e f f . . 
+. . f e 7 f 7 f f 7 f 7 e f . . 
+. . f f f 7 7 e e 7 7 f f f . . 
+. f f e f 7 f e e f 7 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 7 7 1 1 1 1 2 2 4 e . . 
+. . 4 d f 7 1 1 1 1 2 f d 4 . . 
+. . 4 4 f 7 1 1 1 1 2 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image6":
+            case "player_italia":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 8 8 f f f . . . . 
+. . . f f f 8 8 8 8 f f f . . . 
 . . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
+. . f f e 7 1 8 8 1 2 e e f . . 
+. . f e 7 f f f f f f 2 e f . . 
 . . f f f f e e e e f f f f . . 
 . f f e f b f 4 4 f b f e f f . 
 . f e e 4 1 f d d f 1 4 e e f . 
 . . f e e d d d d d d e e f . . 
 . . . f e e 4 4 4 4 e e f . . . 
-. . e 4 f a a a a a a f 4 e . . 
-. . 4 d f a a a a a a f d 4 . . 
-. . 4 4 f 8 8 8 8 8 8 f 4 4 . . 
+. . e 4 f 8 8 7 1 2 8 f 4 e . . 
+. . 4 d f 8 8 7 1 2 8 f d 4 . . 
+. . 4 4 f 8 8 8 1 8 8 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image20":
+            case "player_italia_espalda":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f 8 8 f f f f f . . 
+. . f f e 7 e 8 8 e 2 e f f . . 
+. . f e 7 f 8 f f 8 f 2 e f . . 
+. . f f f 7 8 e e 8 2 f f f . . 
+. f f e f 8 f e e f 8 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 f 8 8 7 1 2 8 f 4 e . . 
+. . 4 d f 8 8 7 1 2 8 f d 4 . . 
+. . 4 4 f 8 8 8 1 8 8 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image9":
+            case "player_paisesbajos":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f a a f f f . . . . 
+. . . f f f a a a a f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 5 5 a a 1 1 e e f . . 
+. . f e 5 f f f f f f 1 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f a a a 1 1 a f 4 e . . 
+. . 4 d f a 5 1 1 5 a f d 4 . . 
+. . 4 4 f a a a 5 5 a f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`;
+            case "image23":
+            case "player_paisesbajos_espalda":return img`
+. . . . . . f f f f . . . . . . 
+. . . . f f e e e e f f . . . . 
+. . . f e e e f f e e e f . . . 
+. . f f f f f a a f f f f f . . 
+. . f f e 5 e a a e 1 e f f . . 
+. . f e 5 f a f f a f 1 e f . . 
+. . f f f 5 a e e a 1 f f f . . 
+. f f e f 5 f e e f 1 f e f f . 
+. f e e f f e e e e f e e e f . 
+. . f e e e e e e e e e e f . . 
+. . . f e e e e e e e e f . . . 
+. . e 4 f a a a 1 1 a f 4 e . . 
+. . 4 d f a 5 5 5 5 a f d 4 . . 
+. . 4 4 f a a a 5 5 a f 4 4 . . 
 . . . . . f f f f f f . . . . . 
 . . . . . f f . . f f . . . . . 
 `;
